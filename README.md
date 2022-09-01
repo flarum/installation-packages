66f352bd6b6deaa687e47c4c6f2c1759b8127660
Sami Mazouz <ilyasmazouz@gmail.com>
2022-09-01 21:37:34 +0100

    Hello World

---
+++ README.md
@@ -0,0 +1,15 @@
+# Flarum Installation Packages
+This repository contains installable package for multiple Flarum versions on different PHP versions. They are auto generated on every new release.
+
+> **Note**
+> The packages come with a bundled package manager, if you do not wish to have the package manager, [install Flarum from the CLI](https://docs.flarum.org/install).
+
+## Download
+* Navigate to the `packages` directory.
+* Navigate to the directory of the desired flarum version.
+* Download the package file associated with your web server's PHP version.
+
+## Usage
+* Extract the package to your web server's document root.
+* Open the website in your web browser.
+* Proceed with the installation process.
---
+++ bin/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -l
+
+# exit on error.
+set -e
+
+style='\e[47;1;31m'
+reset='\e[0;10m'
+
+# Get Flarum Version (transforms "refs/tags/vX.Y.Z" to "vX.Y.Z" then "vX.Y.Z" to "X.Y.Z").
+FLARUM_VERSION = ${GITHUB_SHA//refs\/tags\// } | sed 's/v//g'
+
+# Setup Git.
+echo -e "$style - setting up git $reset"
+git config user.name 'flarum-bot'
+git config user.email 'bot@flarum.org'
+
+# Setup an isolated workspace.
+echo -e "$style - setting up isolated workspace $reset"
+mkdir installation_workspace
+cd installation_workspace
+
+# Install Flarum.
+echo -e "$style - installing Flarum... $reset"
+composer create-project flarum/flarum . --prefer-dist --no-interaction
+
+# Install additional Extensions.
+if [[ "$BUNDLE_VALUE" != "default" ]]; then
+  echo -e "$style - installing bundle $BUNDLE_NAME $reset"
+
+  COMPOSER_PACKAGES = ''
+
+  for p in "$BUNDLE_VALUE"; do
+    COMPOSER_PACKAGES = "${COMPOSER_PACKAGES} p:*"
+  done
+
+  composer require $COMPOSER_PACKAGES --no-interaction
+fi
+
+# Set file name and destination path.
+FILE_NAME = flarum-$(FLARUM_VERSION)-$(BUNDLE_NAME)-php$(PHP_VERSION).tar.gz
+FILE_DESTINATION = packages/flarum-$(FLARUM_VERSION)
+
+# Create installation package.
+cd ../
+tar -czvf $FILE_NAME installation_workspace/*
+
+# Move package to the flarum version folder.
+mkdir -p $FILE_DESTINATION
+mv $FILE_NAME $FILE_DESTINATION/
+
+# Delete workspace.
+rm -R installation_workspace
+
+# Commit package.
+git add $FILE_DESTINATION/*.tar.gz
+git commit -m "Create installation packages for Flarum v$FLARUM_VERSION" -a
+git push
